f9c9d68779330de60d1e831b50edbe72016b92ba
archylex <archylex@protonmail.com>
2020-09-26 16:47:26 +0300

    fix: log(2)

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,10 +4,9 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-    const ln2 = -0.693;
     if (!isNaN(sampleActivity) && typeof sampleActivity === 'string') {
         if (Number(sampleActivity) > 0 && Number(sampleActivity) <= 15)
-            return Math.floor(Math.log(Number(sampleActivity) / MODERN_ACTIVITY) / ln2 * HALF_LIFE_PERIOD);
+            return Math.ceil(Math.log(Number(sampleActivity) / MODERN_ACTIVITY) / -Math.log(2) * HALF_LIFE_PERIOD);
         else return false;
     } else return false;
 };
